d64aaa7e44831912ccad41a81a2691f1a8cd7551
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2019-06-06 20:29:29 -0400

    Initial Commit

---
+++ Day1examples/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day1examples
+//
+//  Created by MacStudent on 2019-06-06.
+//  Copyright © 2019 c0751142. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
